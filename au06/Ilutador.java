5b89980fd97ffafaa896caec95c032b9813841d3
kaue <kaupiovan@gmail.com>
2024-07-22 00:11:54 -0300

    aula 06

---
+++ au06/Ilutador.java
@@ -0,0 +1,9 @@
+package au06;
+
+public interface Ilutador {
+    public abstract void apresentar();
+    public abstract void status();
+    public abstract void ganharLuta();
+    public abstract void perderLutar();
+    public abstract void empatarLuta();
+}
